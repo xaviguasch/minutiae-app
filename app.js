52ac3650b8d514996db9a20c2a61d2ac9f55b8f2
xaviguasch <xaviguasch@gmail.com>
2019-03-12 15:36:42 +0100

    refactored functions to arrow ones

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -42,7 +42,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.removeNote(argv.title)
     }
 })
@@ -51,7 +51,7 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'Lists all your notes',
-    handler: function () {
+    handler() {
         console.log('Listing out all the notes!');
     }
 })
@@ -59,7 +59,7 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Reads all the notes',
-    handler: function () {
+    handler() {
         console.log('Reading all the notes!');
     }
 })
