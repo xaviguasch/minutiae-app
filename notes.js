d505b77f481b570d88e66f49f0912568a8b09e15
xaviguasch <xaviguasch@gmail.com>
2019-03-12 12:25:44 +0100

    used chalk package to provide feedback about the removal of notes

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function () {
     return 'Your notes...'
@@ -25,15 +26,18 @@ const addNote = function (title, body) {
 
 const removeNote = function (title) {
     const notes = loadNotes()
+    const notesRemoved = []
 
     const notesToKeep = notes.filter(function (note) {
         return note.title !== title
     })
 
-    saveNotes(notesToKeep)
-
-
-
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'));
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'));
+    }
 }
 
 const saveNotes = function (notes) {
