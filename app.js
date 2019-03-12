36321eeb820aefb5272fdfce3d97c4e401578098
xaviguasch <xaviguasch@gmail.com>
2019-03-12 09:17:37 +0100

    added and required yargs package

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
 
 const command = process.argv[2]
 
+
+console.log(process.argv);
+
+
 if (command === 'add') {
     console.log('Adding a note');
+} else if (command === 'remove') {
+    console.log('Removing a note');
 }
