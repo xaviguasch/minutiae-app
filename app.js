8e48ad65576cde20a6734b626cf469b4499af596
xaviguasch <xaviguasch@gmail.com>
2019-03-12 11:25:07 +0100

    implemented feature of saving notes to the json file

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log(`Title: ${argv.title}`);
-        console.log(`Body: ${argv.body}`);
+        notes.addNote(argv.title, argv.body)
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,9 +1,34 @@
+const fs = require('fs')
+
 const getNotes = function () {
     return 'Your notes...'
 }
 
 const addNote = function (title, body) {
+    const notes = loadNotes()
 
+    notes.push({
+        title: title,
+        body: body
+    })
+    saveNotes(notes)
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
 
 }
 
